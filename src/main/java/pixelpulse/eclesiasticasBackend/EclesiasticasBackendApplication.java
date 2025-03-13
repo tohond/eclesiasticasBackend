46d589dbfdf54b5a87693b3162ec978cc14c6905
pcSA <pcSA>
2025-03-13 07:27:35 -0500

    Creando repositorio

---
+++ src/main/java/pixelpulse/eclesiasticasBackend/EclesiasticasBackendApplication.java
@@ -0,0 +1,13 @@
+package pixelpulse.eclesiasticasBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclesiasticasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclesiasticasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixelpulse/eclesiasticasBackend/EclesiasticasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pixelpulse.eclesiasticasBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclesiasticasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
