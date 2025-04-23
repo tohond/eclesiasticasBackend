fd9ea9b5fd53cf21394bc73f5b77bbd5fed46f7b
lol <lol@192.168.1.100>
2025-04-23 18:03:14 -0500

    landing endpoint

--- src/main/java/pixelpulse/eclesiasticasbackend/EclesiasticasbackendApplication.java
+++ src/main/java/pixelpulse/eclesiasticasbackend/EclesiasticasbackendApplication.java
@@ -3,7 +3,9 @@ package pixelpulse.eclesiasticasbackend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,8 @@ public class EclesiasticasbackendApplication {
 		SpringApplication.run(EclesiasticasbackendApplication.class, args);
 	}
 	
-	
+	@GetMapping("/")
+    public ResponseEntity<?> home() {
+        return ResponseEntity.ok().body("Inicio satisfactorio");
+    }
 }
