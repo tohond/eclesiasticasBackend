a0b882eebaba18ab5b0ad175d8cd21ba6539108a
omar <omaralexispc@ufps.edu.co>
2025-05-08 18:32:50 -0500

    create

---
+++ src/main/java/pixelpulse/eclesiasticasbackend/dto/create/createActaDTO.java
@@ -0,0 +1,13 @@
+package pixelpulse.eclesiasticasbackend.dto.create;
+
+import java.util.Date;
+
+public class createActaDTO {
+	private String numeroActa;
+    private String folio;
+    private String libro;
+    private Date fecha;
+    private String notas;
+    private String tipo;
+    
+}
---
+++ src/main/java/pixelpulse/eclesiasticasbackend/dto/create/createBautizoDTO.java
@@ -0,0 +1,9 @@
+package pixelpulse.eclesiasticasbackend.dto.create;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class createBautizoDTO extends createActaDTO{
+	
+    
+}
